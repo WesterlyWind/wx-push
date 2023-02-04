a3a0b288696c4c0e57d2f4b8eaa2295f2150972d
WesterlyWind <49097694+WesterlyWind@users.noreply.github.com>
2023-02-04 14:53:58 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
   appid: "wx098cfda9fea1daa7",
   appsecret: "6ae7e5b62ba9bee147ea94cce8aa59a3",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
-  touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
+  template_id: "PbQ0zUtBrsPW8Bmjqpk8OfI65zNLUdYlmcf9ovcnCGE",
+  touser: "oRL5F6kr4F-dc-k2qusSJ45Pll1M",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city: "邢台",
-  first_date: "2022-11-10",
+  city: "上海",
+  first_date: "2021-7-28",
 };
